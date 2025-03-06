b45cdd75e95cd96b8ec36b1490146f39ab19cf89
Luana Maria <59989335+luana-76@users.noreply.github.com>
2025-03-06 13:17:23 -0300

    responsividade part 1

--- front-end/src/components/login/Login.jsx
+++ front-end/src/components/login/Login.jsx
@@ -2,25 +2,39 @@ import './styleLogin.css';
 import './responsive.css';
 import { useState } from "react";
 import { Raiz } from '../google/Raiz';
-
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export function Login(){
 
     const [isChecked, setIsChecked] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);/* Botão de mostrar senha do usuário */
 
     return(
-
         <main id='mainLogin'>
-
             <div id='caixaForm'>
-
                 <h1>Entrar</h1>
-                
                 <form id='formLogin'>
-
+                    
                     <input type='text' placeholder='Email ou telefone' required/>
-                    <input type='password' placeholder='Senha' required/>
+                    
+                    <div className="password-container">
 
+                        <input 
+                            type={showPassword ? 'text' : 'password'}
+                            placeholder='Senha' 
+                            required
+                        />
+
+                        <button 
+                            type="button" 
+                            className="toggle-password"
+                            onClick={() => setShowPassword(!showPassword)}
+                        >
+                            {showPassword ? <FaEyeSlash /> : <FaEye />}
+                        </button>
+
+                    </div>
+                    
                     {/* Botão de ligar e desligar */}
                     <div className="toggle-container">
                         <input
@@ -30,20 +44,14 @@ export function Login(){
                             onChange={() => setIsChecked(!isChecked)}
                         />
                         <label htmlFor="switch" className="toggle-label"></label>
-
-                        <span>Logar automatico</span>
+                        <span>Logar automático</span>
                     </div>
-
+                    
                     <input type='submit'/>
                     <Raiz/>
-
                     <span>Não tem uma conta?<a href='#'> Cadastre-se</a></span>
                 </form>
-
             </div>
-
         </main>
-
     )
-
 }
