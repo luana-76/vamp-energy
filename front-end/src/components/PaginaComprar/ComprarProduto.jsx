de39e33d161a0240c7ee2411e9217191e592f552
Luana Maria <59989335+luana-76@users.noreply.github.com>
2025-04-11 12:48:10 -0300

    mudando url

--- front-end/src/components/PaginaComprar/ComprarProduto.jsx
+++ front-end/src/components/PaginaComprar/ComprarProduto.jsx
@@ -126,7 +126,7 @@ export function ComprarProduto() {
                             
                             className='caixaIcone'
                             onClick={() => { 
-                            fetch('https://vamp-energy-production.up.railway.app/comprar', {
+                            fetch('https://vamp-energy-production.up.railway.app', {
                             method: 'POST',
                             })
                             .then(res => res.json())
