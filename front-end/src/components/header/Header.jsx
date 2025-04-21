cc7c4ab42656f2de4b88fd7d8f5d0cb6449d2b64
Luana Maria <59989335+luana-76@users.noreply.github.com>
2025-04-21 13:36:07 -0300

    Modificações header e  de página produto

--- front-end/src/components/header/Header.jsx
+++ front-end/src/components/header/Header.jsx
@@ -1,53 +1,33 @@
+/* Setando estilos */
 import "./styleHeader.css";
 import "./responsiveHeader.css";
-import Dentadura from "../../assets/logo.png";
+
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export function Header() {
-  const [activeLink, setActiveLink] = useState("Início");
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const [activeLink, setActiveLink] = useState("Início");/* Define o link ativo para destacar na navegação */
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);/* Para o dropDown */
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
-  const [isTablet, setIsTablet] = useState(window.innerWidth >= 768);
 
   const location = useLocation();
 
   useEffect(() => {
+
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 1024);
-      setIsTablet(window.innerWidth >= 768);
+      setIsMobile(window.innerWidth <= 1024); // Define se está em modo mobile ou não
       if (window.innerWidth > 1024) {
-        setIsDropdownOpen(false);
+        setIsDropdownOpen(false); // Fecha o menu dropdown se for desktop
       }
     };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
-  if (location.pathname === "/login" || location.pathname === "/cadastro") {
-    return (
-      <header>
-        <motion.nav
-          initial={{ y: -100, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.6, ease: "easeOut" }}
-          className="sombra"
-        >
-          <Link to="/produtos">
-            <motion.img
-              src={Dentadura}
-              id="logoHeader"
-              alt="logo dentadura"
-              initial={{ y: -100, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{ duration: 0.6, delay: 0.2, ease: "easeOut" }}
-            />
-          </Link>
-        </motion.nav>
-      </header>
-    );
-  }
+    window.addEventListener("resize", handleResize); // Atualiza os estados 'isMobile' e 'isDropdownOpen' com base no tamanho da janela
+
+    return () => window.removeEventListener("resize", handleResize); // Limpa o listener quando o componente for desmontado
+
+  }, []);
 
   return (
     <header
@@ -66,19 +46,10 @@ export function Header() {
         transition={{ duration: 0.6, ease: "easeOut" }}
         className="sombra"
       >
-        {!isMobile && (
-          <Link to="/produtos">
-            <motion.img
-              src={Dentadura}
-              id="logoHeader"
-              alt="logo dentadura"
-              initial={{ y: -100, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{ duration: 0.6, delay: 0.2, ease: "easeOut" }}
-            />
-          </Link>
-        )}
 
+        {/* ========== Ícone e menu dropdown (mobile) ========== */}
+       
+        {/* Para quando estiver no mobile */}
         {!isMobile && (
           <ul id="listaInterna">
             {["Início", "Informações", "Produtos", "Surgimento", "Rodapé"].map(
@@ -125,7 +96,7 @@ export function Header() {
         className="sombra"
       >
         {/* Botão de abrir dropdown */}
-        {(isMobile || isTablet) && (
+        {isMobile && (
           <motion.div
             initial={{ y: -50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
@@ -140,24 +111,25 @@ export function Header() {
                   ? "drop-shadow(0px 0px 10px #ffffff)"
                   : "none",
               }}
+              alt="Ícone de menu"
             />
           </motion.div>
         )}
 
         {/* Overlay escuro ao abrir dropdown */}
-        {(isMobile || isTablet) && isDropdownOpen && (
+        {isMobile && isDropdownOpen && (
           <div className="overlay" onClick={() => setIsDropdownOpen(false)} />
         )}
 
-        {/* Menu dropdown lateral para mobile e tablet */}
-        {(isMobile || isTablet) && isDropdownOpen && (
+        {/* Menu dropdown lateral para mobile */}
+        {isMobile && isDropdownOpen && (
           <motion.div
             id="dropdownMenu"
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.3 }}
+            
           >
-            {/* Menu principal */}
             {["Início", "Informações", "Produtos", "Surgimento", "Rodapé"].map(
               (item, index) => (
                 <motion.div
@@ -165,6 +137,7 @@ export function Header() {
                   initial={{ y: -50, opacity: 0 }}
                   animate={{ y: 0, opacity: 1 }}
                   transition={{ duration: 0.6, delay: 1 + index * 0.2 }}
+                  className={item === "Rodapé" ? "bordaSeparatoria" : "padraoLi"}
                 >
                   <Link
                     to={
@@ -184,6 +157,7 @@ export function Header() {
                       setIsDropdownOpen(false);
                       setActiveLink(item);
                     }}
+
                   >
                     {item}
                   </Link>
@@ -197,29 +171,49 @@ export function Header() {
               animate={{ y: 0, opacity: 1 }}
               transition={{ duration: 0.6, delay: 2.2 }}
             >
+
               <Link to="/login" onClick={() => setIsDropdownOpen(false)}>
                 Entrar
               </Link>
+
             </motion.div>
             <motion.div
               initial={{ y: -50, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ duration: 0.6, delay: 2.4 }}
             >
+
               <Link to="/cadastro" onClick={() => setIsDropdownOpen(false)}>
                 Cadastrar
               </Link>
+
             </motion.div>
+            
           </motion.div>
         )}
 
-        {/* LINKS "Entrar" e "Cadastrar" no desktop */}
-        {!isMobile && !isTablet && (
-          <div className="auth-desktop">
-            <Link to="/login">Entrar</Link>
-            <Link to="/cadastro">Cadastrar</Link>
+        {/* ========== Navegação principal (desktop) ========== */}
+        {/* ========== Links "Entrar" e "Cadastrar" (desktop) ========== */}
+
+        {!isMobile && (
+          <div className="auth-desktop" style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAWVJREFUSEvFlM8rRFEUxz/fZI2wsJxkZaPYytKGmqLY2NjZWPi1tFFKiaT8CbKRX9n4ByjJQrMiS1JYoJTimJnmTePNe3Nf983k7t6793w+59zOuaLBSw3mk0hgZkPAKtBbSugGWJJ07krQKTCzSWAXqpL5AbKSTmpJagrMrBW4B9piII9At6TPOIlLkAUOHNcwLOnMVzAPrDsEc5I2fQWjwLFDMCLp1FfQAtwBHTGAJ6BH0ruXoBBkZhPAXgSg0EVjkg69uygINLNBYK00B99ADliUdJF6DlwA175z0FwA136swMw6gQWgH+gD2kOwF+AauAQ2JD1HySIFZjaVB24DhS5Ksl6BaUlH4cNVAjObzT8NW0moEWfGJe1X/v8jMLMMcAs0eQo+gEzldYUFO8CMJzwIW5a0EnyEBQ9AV0rBlaSBOMEX0JxS8JZ//MrNEa7AUsKL4ZLK3P8btHpUUqymXqA4zi95YmAZ3ClLGQAAAABJRU5ErkJggg=="
+              alt="Ícone de menu"
+              style={{ width: "30px", height: "30px" }}
+            />
+
+            <div className='desktop-links'>
+
+              <Link to="/login">Entrar</Link>
+              <Link to="/cadastro">Cadastrar</Link>
+
+            </div>
+      
           </div>
         )}
+
       </motion.nav>
     </header>
   );
