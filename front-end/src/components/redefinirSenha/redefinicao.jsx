f054ef141c20cce3160e500428e4118b301d2882
Luana Maria <59989335+luana-76@users.noreply.github.com>
2025-06-27 10:08:24 -0300

    ajeitando layout da página de redefinir senha

--- front-end/src/components/redefinirSenha/redefinicao.jsx
+++ front-end/src/components/redefinirSenha/redefinicao.jsx
@@ -9,6 +9,7 @@ export function Redefinicao() {
   const [novaSenha, setNovaSenha] = useState('');
   const [confirmarSenha, setConfirmarSenha] = useState('');
 
+  //Contato com o back
   function redefinirSenha() {
     if (novaSenha !== confirmarSenha) {
       alert('As senhas não coincidem!');
@@ -34,48 +35,58 @@ export function Redefinicao() {
         console.error('Erro:', error);
         alert('Erro ao redefinir senha.');
       });
+
   }
 
   return (
     <main id='mainRedefinicao'>
+
       <form className='formRedefinicao' onSubmit={(e) => e.preventDefault()}>
+
         <h1>Redefinir Senha</h1>
 
-        <label>Informe seu email</label>
+        <label htmlFor="email">Informe seu email</label>
         <input
           type='email'
           placeholder='Digite seu email'
           required
+          id='email'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
 
-        <label>Nova senha</label>
+        <label htmlFor='senha1'>Nova senha</label>
         <input
           type='password'
           placeholder='Digite sua nova senha'
           required
+          id='senha1'
           value={novaSenha}
           onChange={(e) => setNovaSenha(e.target.value)}
         />
 
-        <label>Confirmar senha</label>
+        <label htmlFor="senha2">Confirmar senha</label>
         <input
           type='password'
           placeholder='Confirme a senha'
           required
+          id='senha2'
           value={confirmarSenha}
           onChange={(e) => setConfirmarSenha(e.target.value)}
         />
 
         <input type='button' value='Redefinir' onClick={redefinirSenha} />
+
       </form>
+
+      {/* Parte das imagens */}
       <div id='imagemPart'>
 
-        <img src={adolescente} alt='imagemAdo'/>
-        <img src={tradicional} alt='imagemTradi'/>
+        <img src={adolescente} alt='imagemAdolescente'/>
+        <img src={tradicional} alt='lataTradicional'/>
 
       </div>
+
     </main>
   );
 }
